2b3d310fbbf913160155b7bc867c4e4a0593eeda
Pichet <pcwc2000@hotmail.com>
2015-12-22 16:42:55 +0700

    ขั้น 4

--- app/src/main/java/appewtc/masterung/mytraffic/DetailActivity.java
+++ app/src/main/java/appewtc/masterung/mytraffic/DetailActivity.java
@@ -15,12 +15,36 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.detail_info);
 
         //Bind Widgget
         bindWidget();
 
+        //Show view
+        showView();
+
     }   //main method
+
+    private void showView() {
+
+        //Show title
+        String strTitle = getIntent().getStringExtra("Title");
+        titleTextView.setText(strTitle);
+
+        //show Image
+        int intIcon = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        picImageView.setImageResource(intIcon);
+
+        //show detail
+        String[] strDetail = getResources().getStringArray(R.array.detail_long);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+        dataTextView.setText(strDetail[intIndex]);
+
+
+
+
+
+    }   //showView
 
     private void bindWidget() {
         titleTextView = (TextView) findViewById(R.id.textView4);
